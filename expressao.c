b3b348cbf49021aa47d88214a4e731a5e26078ad
Lucas Ataide <163359192+Lucas200507@users.noreply.github.com>
2025-06-22 21:29:37 -0300

    Update expressao.c

--- expressao.c
+++ expressao.c
@@ -3,8 +3,8 @@
 #include "expressao.h"
 
 Pilha* criarPilha(){
-    Pilha* p = malloc(sizeof(Pilha));
-    p->topo = NULL;
+    Pilha* p = malloc(sizeof(Pilha)); // alocação da estrutura pilha
+    p->topo = NULL; // cria o topo como null
     p->tamanho = 0;
     return p;
 }
@@ -19,13 +19,44 @@ int verificarOpcao(){
     return opcao;
 }
 
+// PUSH
 void empilhar(Pilha* p, char valor){    
     No* novo = malloc(sizeof(No));
-    novo->dado = valor;   
-    novo->prox = p->topo;
-    p->topo = novo;
-    p->tamanho++;
+    if (novo){
+        novo->dado = valor;   
+        novo->prox = p->topo;
+        p->topo = novo;
+        p->tamanho++;
+    } else {
+        printf("Erro na alocação.\n");        
+    }
 }
+
+// POP
+float desempilhar(Pilha* p){
+   if (p->topo == NULL){
+    printf('A pilha está vazia');
+    return -1;
+   } else {
+    No* temp = p->topo; // guardar o dado do topo
+    float valor = temp->dado;
+    p->topo = temp->prox;
+    free(temp); // libera na memória o nó temporário
+    return valor; // retorna o valor desempilhado da lista
+
+   }
+}
+
+void mostrarPilha(Pilha *p){
+    No* temp = p->topo;
+    printf("TOPO -> ");
+    while (temp != NULL){
+        printf(" %f ->", temp->dado);
+        temp = temp->prox;
+    }
+}
+
+// -------------------- OUTRAS OPERAÇÕES -------------------- 
 
 void converte(Pilha* p, int opcao){
     // VERIFICA QUAL CONVERSÃO VOCÊ ESCOLHEU
@@ -38,40 +69,40 @@ void converte(Pilha* p, int opcao){
 
 // Vai receber uma lista de operação Infixada e converter para posFixada
 // (3 + 4) * 5
-void posFixada(Pilha* p){
-    bool parenteses;
-    No* atual = p->topo;
-    // Percorrer toda a lista, verifica se tem ()
-    while (atual != NULL){
-        if (atual->dado == '('){
-            parenteses = true;
-            break;
-        }
-        atual = atual->prox;
-    }
-    // se achar, precisa empilhar o próximo número
-    if (parenteses){
-        No* posFixada = malloc(sizeof(No));
-        // empilhando, 
-        posFixada->dado = atual->ant->dado;
-        posFixada->prox = p->topo;
-        p->topo = posFixada;
-        No* temp = p->topo;
-        while (posFixada != NULL){
-            // precisa verificar se é um operador
-            if (posFixada->dado == '+' || posFixada->dado == '-' || posFixada->dado == '*' || posFixada->dado == '/'){
-                temp = posFixada;
-                posFixada = posFixada->prox;
-                posFixada->prox = temp;
-            // Pula quando o nó for um ), a lista posFixada não possui parênteses
-            } else if (posFixada->dado == ')'){
-                posFixada = posFixada->prox;
-            }
-        }
+// void posFixada(Pilha* p){
+//     bool parenteses;
+//     No* atual = p->topo;
+//     // Percorrer toda a lista, verifica se tem ()
+//     while (atual != NULL){
+//         if (atual->dado == '('){
+//             parenteses = true;
+//             break;
+//         }
+//         atual = atual->prox;
+//     }
+//     // se achar, precisa empilhar o próximo número
+//     if (parenteses){
+//         No* posFixada = malloc(sizeof(No));
+//         // empilhando, 
+//         posFixada->dado = atual->ant->dado;
+//         posFixada->prox = p->topo;
+//         p->topo = posFixada;
+//         No* temp = p->topo;
+//         while (posFixada != NULL){
+//             // precisa verificar se é um operador
+//             if (posFixada->dado == '+' || posFixada->dado == '-' || posFixada->dado == '*' || posFixada->dado == '/'){
+//                 temp = posFixada;
+//                 posFixada = posFixada->prox;
+//                 posFixada->prox = temp;
+//             // Pula quando o nó for um ), a lista posFixada não possui parênteses
+//             } else if (posFixada->dado == ')'){
+//                 posFixada = posFixada->prox;
+//             }
+//         }
         
-    }
+//     }
     
-}
+// }
 
 // Exibir o resultado
-void desempilhar(Pilha* p, int opcao){}
+// void desempilhar(Pilha* p, int opcao){}
